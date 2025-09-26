a97276d4af497b99471cfe821588aa52ea3ccb1d
Admin <Admin@DESKTOP-QUC5C0J>
2025-09-26 23:11:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Работу выполнили Дайнеко и Гергель 3832
+
+Console.WriteLine("Работу выполнили Дайнеко и Гергель 3832");
+Console.WriteLine();
+
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+
+Console.Write("Введите фамилию: ");
+string surname = Console.ReadLine();
+
+Console.Write("Введите год рождения: ");
+string yearText = Console.ReadLine();
+
+int year = int.Parse(yearText);
+int age = 2024 - year;
+
+Console.WriteLine();
+Console.WriteLine($"Добавлен пользователь {name} {surname}, возраст = {age}");
+
+Console.WriteLine("Нажмите Enter чтобы выйти...");
+Console.ReadLine();
