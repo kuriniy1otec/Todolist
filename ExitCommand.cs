62ca41ee6c6b4d4895951c57995699da1ac92f0d
kuriniy1otec <vladdayneko07@mail.ru>
2025-11-01 07:27:03 +0300

    lesson 7/Созданы служебные команды help и exit

---
+++ ExitCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Todolist
+{
+    public class ExitCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Выход из программы...");
+            Environment.Exit(0);
+        }
+    }
+}
